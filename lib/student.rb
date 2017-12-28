a3216a19d98a84455c6d8c321885bd3ad52d067e
Sherry El Garhy <sherryptk@gmail.com>
2017-12-28 22:52:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,13 @@ class Student
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+  
   end
 
 
