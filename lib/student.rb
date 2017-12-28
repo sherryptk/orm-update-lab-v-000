455edca68a2f0d8bc9da50958e330aa59a26f67d
Sherry El Garhy <sherryptk@gmail.com>
2017-12-28 23:04:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade, id=nil)
-   @id = id
+  def initialize(name, grade)
+   @id = nil
    @name = name
    @grade = grade
  end
