8eb07aa4d3be4ab2eaf888db5839345cdca2851c
Sherry El Garhy <sherryptk@gmail.com>
2017-12-28 22:56:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,6 +49,12 @@ class Student
     students
   end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+
   def update
     sql = "UPDATE songs SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
