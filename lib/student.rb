51e14095e51c3f72d1e1b492c037688194bb4053
Sherry El Garhy <sherryptk@gmail.com>
2017-12-28 23:08:49 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,12 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
+  def self.new_from_db(row)
+  new_student = self.new  # self.new is the same as running Song.new
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.length = row[2]
+  new_student  # return the newly created instance
+end
+
   end
